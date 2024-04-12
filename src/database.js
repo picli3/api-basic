870f0473f27027749fd67fe04c0e7152a94b0840
Maykol Rey <maykolrey@gmail.com>
2024-04-12 06:41:50 -0500

    agregando docker compose, se cambio la ruta del servidor de api en database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,6 @@ import e from "express";
 import mongoose from "mongoose";
 
 mongoose
-  .connect("mongodb://maykol:123456@localhost:27017/companydb?authSource=admin")
-
+  .connect("mongodb://maykol:123456@mongo-api:27017/companydb?authSource=admin")
   .then((db) => console.log("db conectado"))
   .catch((e) => console.log(e));
